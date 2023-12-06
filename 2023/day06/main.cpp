f388ecf5d80e4016a5826bfc011959feaa19bb31
Ichiro Misumi <124260839+imisumi@users.noreply.github.com>
2023-12-05 23:23:05 -0800

    day06

---
+++ 2023/day06/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+std::vector<std::string> readFileLines(const std::string& filename)
+{
+	std::vector<std::string> lines;
+	std::ifstream file(filename);
+
+	if (file.is_open())
+	{
+		std::string line;
+		while (std::getline(file, line))
+			lines.push_back(line);
+		file.close();
+	}
+	else
+		std::cerr << "Unable to open file: " << filename << std::endl;
+	return lines;
+}
+
+uint64_t maxWinRate(uint64_t time, uint64_t distToBeat)
+{
+	uint64_t wins = 0;
+	for (int i = time - 1; i > 0; i--)
+	{
+		uint64_t dist = i * (time - i);
+		wins += (dist > distToBeat) ? 1 : 0;
+	}
+	return wins;
+}
+
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		std::cout << "give input file as argument" << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::vector<std::string> fileLines = readFileLines(argv[1]);
+
+	std::vector<int> part_one_times;
+	std::vector<int> part_one_dist;
+	for (int i = 0; i < fileLines.size(); i++)
+	{
+		for (int j = 0; j < fileLines[i].size(); j++)
+		{
+			while (std::isalpha(fileLines[i][j]))
+				j++;
+			while (!std::isdigit(fileLines[i][j]))
+				j++;
+			uint64_t num = 0;
+			while (std::isdigit(fileLines[i][j]))
+			{
+				num *= 10;
+				num += fileLines[i][j] - '0';
+				j++;
+			}
+			if (i == 0)
+				part_one_times.push_back(num);
+			else
+				part_one_dist.push_back(num);
+		}
+	}
+	//! PART ONE
+	uint64_t part_one = 1;
+	for (int i = 0; i < part_one_times.size(); i++)
+		part_one = part_one * maxWinRate(part_one_times[i], part_one_dist[i]);
+	std::cout << "part 1 = " << part_one << std::endl;
+
+	//! PART TWO
+	uint64_t part_two_input[2];
+	for (int i = 0; i < fileLines.size(); i++)
+	{
+		std::string str = "";
+		for (int j = 0; j < fileLines[i].size(); j++)
+		{
+			if (std::isdigit(fileLines[i][j]))
+				str += fileLines[i][j];
+		}
+		part_two_input[i] = std::stoull(str);
+	}
+	std::cout << "part 2 = " << maxWinRate(part_two_input[0], part_two_input[1]) << std::endl;
+	return EXIT_SUCCESS;
+}
